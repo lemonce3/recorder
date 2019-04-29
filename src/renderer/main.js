839e8751f6b84d41959694672031bf3eed9e5312
DameNeko <phoensxar@gmail.com>
2019-04-29 20:03:36 +0800

    [FIXED] eslintrc

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from 'vue';
+import axios from 'axios';
 
-import App from './App'
-import router from './router'
-import store from './store'
+import App from './App';
+import router from './router';
+import store from './store';
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
-Vue.config.productionTip = false
-Vue.config.devtools = true
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
-  router,
-  store,
-  template: '<App/>'
-}).$mount('#app')
+	components: { App },
+	router,
+	store,
+	template: '<App/>'
+}).$mount('#app');
